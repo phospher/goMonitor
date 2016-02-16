cceeea1b244ad9df0068bc60fa0b4acc6e754d1e
Phospher Lau <phospher@qq.com>
2016-02-16 20:03:26 +0800

    read config.ini

--- agent/src/monitorAgent/main.go
+++ agent/src/monitorAgent/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"github.com/go-ini/ini"
+)
+
+const (
+	MainServerSectionName = "MainServer"
+	MainServerIpKeyName   = "IP"
+	MainServerPortKeyName = "Port"
+)
 
 func main() {
-	fmt.Println("monitorAgent")
+	address, err := getMainServer()
+	if err == nil {
+		fmt.Println(address)
+	} else {
+		fmt.Println(err.Error())
+	}
+}
+
+func getMainServer() (string, error) {
+	cfg, err := ini.Load("config.ini")
+	if err != nil {
+		return "", err
+	}
+
+	if session, err := cfg.GetSection(MainServerSectionName); err == nil {
+		ip, err := getConfigValue(session, MainServerIpKeyName)
+		if err != nil {
+			return "", err
+		}
+		port, err := getConfigValue(session, MainServerPortKeyName)
+		if err != nil {
+			return "", err
+		}
+
+		return fmt.Sprintf("%s:%s", ip, port), nil
+	} else {
+		return "", errors.New("Can not find MainServer configuration")
+	}
+}
+
+func getConfigValue(session *ini.Section, key string) (string, error) {
+	if session.HasKey(key) {
+		return session.Key(key).String(), nil
+	} else {
+		return "", fmt.Errorf("Can not find %s configuration", key)
+	}
 }
