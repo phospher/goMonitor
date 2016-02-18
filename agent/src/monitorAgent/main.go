6ecd48c5b37685c00535c6eb054d21574ab88f59
Phospher Lau <phospher@qq.com>
2016-02-18 11:38:34 +0800

    get process status

--- agent/src/monitorAgent/main.go
+++ agent/src/monitorAgent/main.go
@@ -6,5 +6,5 @@ import (
 )
 
 func main() {
-	fmt.Println(GetSystemMemoryUsage())
+	fmt.Println(GetSystemInfo())
 }
--- agent/src/monitorAgent/systemInfo.go
+++ agent/src/monitorAgent/systemInfo.go
@@ -1,13 +1,40 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
+	"fmt"
 	"net"
 	"os/exec"
 	"regexp"
 	"strconv"
 )
 
-func GetMacAddress() string {
+type ProcessInfo struct {
+	Pid         int
+	ProcessName string
+	CPUUsage    float64
+	MemoryUsage float64
+}
+
+type SystemInfo struct {
+	MacAddress    string
+	CPUUsage      float64
+	MemoryUsage   float64
+	ProcessStates []ProcessInfo
+}
+
+func GetSystemInfo() SystemInfo {
+	result := SystemInfo{}
+	result.MacAddress = getMacAddress()
+	topOutput := getTopCommandOutput()
+	result.CPUUsage = getSystemCPUUsage(topOutput)
+	result.MemoryUsage = getSystemMemoryUsage(topOutput)
+	getProcessStates(topOutput)
+	return result
+}
+
+func getMacAddress() string {
 	interfaces, err := net.Interfaces()
 	if err != nil {
 		return err.Error()
@@ -15,37 +42,48 @@ func GetMacAddress() string {
 	return interfaces[1].HardwareAddr.String()
 }
 
-func GetSystemCPUUsage() float64 {
+func getTopCommandOutput() []byte {
 	cmd := exec.Command("top", "-bn1")
-	if output, err := cmd.Output(); err == nil {
-		regex, _ := regexp.Compile(`%?Cpu\(s\).*id`)
-		targetLine := regex.Find(output)
-		lineRegex, _ := regexp.Compile(`(([0-9]|\.)*)%? *id`)
-		resultStr := lineRegex.FindSubmatch(targetLine)
-		if len(resultStr) < 2 {
-			return 0
-		} else {
-			result, _ := strconv.ParseFloat(string(resultStr[1]), 64)
-			return (100 - result) / 100
-		}
+	if result, err := cmd.Output(); err == nil {
+		return result
 	} else {
-		return 0
+		return make([]byte, 0)
 	}
 }
 
-func GetSystemMemoryUsage() float64 {
-	cmd := exec.Command("top", "-bn1")
-	if output, err := cmd.Output(); err == nil {
-		regex, _ := regexp.Compile(`Mem: *([0-9]*)[A-Za-z]* *total, *([0-9]*)[A-Za-z]* *used`)
-		resultStr := regex.FindSubmatch(output)
-		if len(resultStr) < 3 {
-			return 0
-		} else {
-			totalMem, _ := strconv.ParseFloat(string(resultStr[1]), 64)
-			usedMem, _ := strconv.ParseFloat(string(resultStr[2]), 64)
-			return usedMem / totalMem
-		}
-	} else {
+func getSystemCPUUsage(output []byte) float64 {
+	regex, _ := regexp.Compile(`%?Cpu\(s\).*id`)
+	targetLine := regex.Find(output)
+	lineRegex, _ := regexp.Compile(`(([0-9]|\.)*)%? *id`)
+	resultStr := lineRegex.FindSubmatch(targetLine)
+	if len(resultStr) < 2 {
 		return 0
+	} else {
+		result, _ := strconv.ParseFloat(string(resultStr[1]), 64)
+		return (100 - result) / 100
 	}
+}
+
+func getSystemMemoryUsage(output []byte) float64 {
+	regex, _ := regexp.Compile(`Mem: *([0-9]*)[A-Za-z]* *total, *([0-9]*)[A-Za-z]* *used`)
+	resultStr := regex.FindSubmatch(output)
+	if len(resultStr) < 3 {
+		return 0
+	} else {
+		totalMem, _ := strconv.ParseFloat(string(resultStr[1]), 64)
+		usedMem, _ := strconv.ParseFloat(string(resultStr[2]), 64)
+		return usedMem / totalMem
+	}
+}
+
+func getProcessStates(output []byte) []byte {
+	scanner := bufio.NewScanner(bytes.NewReader(output))
+	scanner.Split(bufio.ScanLines)
+	regex, _ := regexp.Compile(`[0-9]+.*chrome`)
+	for scanner.Scan() {
+		if regex.MatchString(scanner.Text()) {
+			fmt.Println(scanner.Text())
+		}
+	}
+	return make([]byte, 0)
 }
