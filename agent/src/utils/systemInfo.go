f9a0b39ad500437a7d24c0eefcfc9daa64dd121e
Phospher Lau <phospher@qq.com>
2016-05-03 10:26:14 +0800

    update SysteInfo struct tags

--- agent/src/utils/systemInfo.go
+++ agent/src/utils/systemInfo.go
@@ -1,14 +1,14 @@
 package utils
 
 type ProcessInfo struct {
-	ProcessName string
-	CPUUsage    float64
-	MemoryUsage float64
+	ProcessName string `ProcessName`
+	CPUUsage    float64 `CPUUsage`
+	MemoryUsage float64 `MemoryUsage`
 }
 
 type SystemInfo struct {
-	MacAddress    string
-	CPUUsage      float64
-	MemoryUsage   float64
-	ProcessStates []*ProcessInfo
+	MacAddress    string `MacAddress`
+	CPUUsage      float64 `CPUUsage`
+	MemoryUsage   float64 `MemoryUsage`
+	ProcessStates []*ProcessInfo `ProcessStates`
 }
