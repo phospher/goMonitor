6e753c44ce04338bfec2f6043f191aab9ecb41c5
Phospher Lau <phospher@qq.com>
2016-11-01 11:31:33 +0800

    arguments for configuration file path

--- agent/src/utils/config.go
+++ agent/src/utils/config.go
@@ -1,11 +1,22 @@
 package utils
 
 import (
+	"flag"
 	"fmt"
-	"github.com/go-ini/ini"
 	"os"
 	"path/filepath"
+
+	"github.com/go-ini/ini"
 )
+
+var configFilePath string
+
+func init() {
+	currentDir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+	defaultConfigFilePath := filepath.Join(currentDir, "config.ini")
+	flag.StringVar(&configFilePath, "configFile", defaultConfigFilePath, "Configuration File Path")
+	flag.Parse()
+}
 
 func GetConfigValue(session *ini.Section, key string) (string, error) {
 	if session.HasKey(key) {
@@ -16,6 +27,5 @@ func GetConfigValue(session *ini.Section, key string) (string, error) {
 }
 
 func GetConfigFilePath() string {
-	dirPath, _ := filepath.Abs(filepath.Dir(os.Args[0]))
-	return filepath.Join(dirPath, "config.ini")
+	return configFilePath
 }
